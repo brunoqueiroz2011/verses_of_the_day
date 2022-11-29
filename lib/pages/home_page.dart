0403876195ac827f7ae3a13efeca76101088227a
DIR\bruno.g.queiroz <bruno.g.queiroz@accenture.com>
2022-11-29 12:15:57 -0300

    Ajuste para dar um retorno visual para o usuario enquanto consulta a api

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,14 +12,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late var _randomVerse = "Clique Abaixo para gerar um versiculo!";
   var _reference = "";
+  final loading = ValueNotifier<bool>(false);
 
   void generateVerse() async {
+    loading.value = true;
     var ws = WebServiceAPI();
     Verses verse = await ws.getVersesRandom();
     setState(() {
       _randomVerse = verse.text.toString();
       _reference =
           '${verse.book?.name} ${verse.chapter}:${verse.number}\n${verse.book?.version?.toUpperCase()}';
+      loading.value = false;
     });
   }
 
@@ -50,19 +53,27 @@ class _HomePageState extends State<HomePage> {
               textAlign: TextAlign.center,
             ),
             ElevatedButton(
-              onPressed: generateVerse,
+              onPressed: !loading.value ? generateVerse : null,
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
                 minimumSize: MaterialStateProperty.all(const Size(200, 50)),
               ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
-              child: const Text(
-                "Novo Versiculo",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
+              child: AnimatedBuilder(
+                  animation: loading,
+                  builder: (context, child) => loading.value
+                      ? const SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: CircularProgressIndicator(color: Colors.white),
+                        )
+                      : const Text(
+                          "Novo Versiculo",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        )),
             ),
           ],
         ),
